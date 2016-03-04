aabfad04b5b9e4d8eb1807b8da39c47dea5b634f
brnelsons <brnelsons139@gmail.com>
2016-03-04 00:22:43 -0600

    added settings, added styles, changed Mon to Monday etc, added debugging flag

--- js/myScripts.js
+++ js/myScripts.js
@@ -4,23 +4,38 @@
 window.$ = window.jQuery = require('./js/jquery.js');
 
 $(document).ready(function () {
+
+    var shortToLong = {
+        'Now': 'Now',
+        'Today': 'Today',
+        'Mon': 'Monday',
+        'Tue': 'Tuesday',
+        'Wed': 'Wednesday',
+        'Thu': 'Thursday',
+        'Fri': 'Friday',
+        'Sat': 'Saturday',
+        'Sun': 'Sunday'
+    }
+
     function appendWeatherCard(day, label, current, high, low, imageUrl, degUnit) {
         var div = "<div class='weather-card col-xs-2'>";
-        div += "<div class='day-label'>" + day + "</div>";
+        div += "<div class='bold centered-label'>" + shortToLong[day] + "</div>";
         if (current != null && current !== undefined)
-            div += "<div>" + current + degUnit + "</div>";
+            div += "<div class='centered-label'><span class='bold'>" + current + "</span>" + degUnit + "</div>";
         if (high != null && high !== undefined && low != null && low !== undefined) {
-            div += "<div>";
-            div += "<span style='color:#0091EA'>"+low+"</span>";
+            div += "<div class='centered-label'>";
+            div += "<span class='bold cold'>"+low+"</span>";
             div += "<span>"+degUnit+"</span>";
             div += "<span>/</span>";
-            div += "<span style='color:#D50000'>"+high+"</span>";
+            div += "<span class='bold warm'>"+high+"</span>";
             div += "<span>"+degUnit+"</span>";
             div += "</div>";
         }
+        div += "<div class='weather-img-div'>";
         div += "<img src=\"" + imageUrl + "\" />";
+        div += "</div>";
         if (label != null && label !== undefined)
-            div += "<div>" + label + "</div>";
+            div += "<div class='centered-label'>" + label + "</div>";
         div += "</div>";
         $('#main-row').append(div);
     }
@@ -56,85 +71,3 @@ $(document).ready(function () {
         });
     });
 });
-
-// sample weather JSON
-//[
-//    {
-//        "location": {
-//            "name": "Wentzville, MO",
-//            "lat": "38.812",
-//            "long": "-90.854",
-//            "timezone": "-6",
-//            "alert": "",
-//            "degreetype": "F",
-//            "imagerelativeurl": "http://blob.weather.microsoft.com/static/weather4/en-us/"
-//        },
-//        "current": {
-//            "temperature": "42",
-//            "skycode": "9",
-//            "skytext": "Light Rain",
-//            "date": "2016-03-03",
-//            "observationtime": "12:20:00",
-//            "observationpoint": "Wentzville, MO",
-//            "feelslike": "38",
-//            "humidity": "92",
-//            "winddisplay": "6 mph Northeast",
-//            "day": "Thursday",
-//            "shortday": "Thu",
-//            "windspeed": "6 mph",
-//            "imageUrl": "http://blob.weather.microsoft.com/static/weather4/en-us/law/9.gif"
-//        },
-//        "forecast": [
-//            {
-//                "low": "38",
-//                "high": "45",
-//                "skycodeday": "27",
-//                "skytextday": "Cloudy",
-//                "date": "2016-03-02",
-//                "day": "Wednesday",
-//                "shortday": "Wed",
-//                "precip": ""
-//            },
-//            {
-//                "low": "33",
-//                "high": "46",
-//                "skycodeday": "26",
-//                "skytextday": "Cloudy",
-//                "date": "2016-03-03",
-//                "day": "Thursday",
-//                "shortday": "Thu",
-//                "precip": "90"
-//            },
-//            {
-//                "low": "41",
-//                "high": "50",
-//                "skycodeday": "30",
-//                "skytextday": "Partly Sunny",
-//                "date": "2016-03-04",
-//                "day": "Friday",
-//                "shortday": "Fri",
-//                "precip": "0"
-//            },
-//            {
-//                "low": "32",
-//                "high": "57",
-//                "skycodeday": "32",
-//                "skytextday": "Sunny",
-//                "date": "2016-03-05",
-//                "day": "Saturday",
-//                "shortday": "Sat",
-//                "precip": "0"
-//            },
-//            {
-//                "low": "46",
-//                "high": "57",
-//                "skycodeday": "28",
-//                "skytextday": "Mostly Cloudy",
-//                "date": "2016-03-06",
-//                "day": "Sunday",
-//                "shortday": "Sun",
-//                "precip": "60"
-//            }
-//        ]
-//    }
-//]
--- main.js
+++ main.js
@@ -6,23 +6,26 @@ const electron = require('electron');
 const app = electron.app;
 const Menu = electron.Menu;
 const MenuItem = electron.MenuItem;
+const BrowserWindow = electron.BrowserWindow;
+
+var DEBUG_ENABLED = true;
 
 var moment = require('moment');
 var menubar = require('menubar');
 var ipcMain = require('electron').ipcMain;
 var path = require('path');
-var cachedBounds; // cachedBounds are needed for double-clicked event
-
 var weather = require("yahoo-weather");
+var cachedBounds; // cachedBounds are needed for double-clicked event
 
 var opts = {
     dir: __dirname,
-    icon: path.join(__dirname, 'images', 'dark-sunny.png'),
+    icon: path.join(__dirname, 'images', 'light-sunny.png'),
     width: 800,
     height: 150,
     tooltip: 'Show Weather',
     transparent: "true",
-    'preload-window': 'true'
+    'preload-window': 'true',
+    'always-on-top': 'true'
 };
 
 var mb = menubar(opts);
@@ -33,8 +36,10 @@ mb.on('ready', function ready() {
     var tray = mb.tray;
     var contextMenu = new Menu();
     contextMenu.append(new MenuItem({label: "Show", click: clicked}));
-    //contextMenu.append(new MenuItem({label: "Update", click: util.update}));
-    contextMenu.append(new MenuItem({label: "Debug", click: util.debug}));
+    contextMenu.append(new MenuItem({label: "Settings", click: util.showSettings}));
+    if (DEBUG_ENABLED) {
+        contextMenu.append(new MenuItem({label: "Debug", click: util.debug}));
+    }
     contextMenu.append(new MenuItem({
         label: "Quit", click: function () {
             app.quit()
@@ -45,33 +50,35 @@ mb.on('ready', function ready() {
 
 mb.on('after-create-window', function onCreateWindow() {
     mb.window.webContents.on('dom-ready', function () {
-        //weather.find({search: 'Wentzville, MO', degreeType: 'F'}, function(err, result) {
-        //    if(err) console.log(err);
-        //    mb.window.webContents.send('ping', JSON.stringify(result, null, 2));
-        //});
-        weather({q: 'Wentzville, MO'}).then(function(result){
-            //console.log(JSON.stringify(result, null, 2));
+        weather({q: 'Wentzville, MO'}).then(function (result) {
             mb.window.webContents.send('ping', JSON.stringify(result, null, 2));
         });
     });
 });
 
 function clicked(e, bounds) {
-    if (e.altKey || e.shiftKey || e.ctrlKey || e.metaKey) return mb.hideWindow();
-    if (menubar.window && menubar.window.isVisible()) return mb.hideWindow();
+    if (DEBUG_ENABLED) {
+        //disable auto hide functions
+        if (e.altKey || e.shiftKey || e.ctrlKey || e.metaKey) return mb.hideWindow();
+        if (menubar.window && menubar.window.isVisible()) return mb.hideWindow();
+    }
     cachedBounds = bounds || cachedBounds;
     mb.showWindow(cachedBounds)
 }
 //
 var util = {
-    //update: function getWeather(e, message) {
-    //    if (e.altKey || e.shiftKey || e.ctrlKey || e.metaKey) return mb.hideWindow();
-    //    if (mb.window) return mb.hideWindow();
-    //    weather.find({search: 'Wentzville, MO', degreeType: 'F'}, function (err, result) {
-    //        if (err) console.log(err);
-    //        console.log(JSON.stringify(result, null, 2));
-    //    });
-    //},
+    showSettings: function settings() {
+        var options = {
+            height:200,
+            width:400,
+            show: true,
+            frame: true,
+            center: true,
+            darkTheme: true
+        };
+        var newWindow = new BrowserWindow(options);
+        newWindow.loadURL('file://' + path.join(opts.dir, 'settings.html'))
+    },
 
     debug: function debug() {
         mb.window.toggleDevTools();
